3bdba39dcd938a8d567241e40161ded805907c4d
Gabriel <gabriel.jauma@gmail.com>
2023-07-26 15:54:19 +0200

    Created SQcircuit_extensions.py

---
+++ Modules/SQcircuit_extensions.py
@@ -0,0 +1,72 @@
+import SQcircuit as sq
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams['backend'] = 'QtAgg'
+
+#%%
+def truncation_convergence(circuit, n_eig, trunc_nums=False, threshold=1e-3, refine=True, plot=False):
+    '''
+    This function tests the convergence of set_trunc_nums.
+    It increases the truncation numbers until the convergence condition is met.
+    The convergence condition is that the max difference between the spectrum calculated using some truncation numbers,
+    versus those truncation numbers + 1, must be below a certain "threshold" (given by user or set as 0.1%).
+
+    If refine is True then it reduces the truncation number of each mode iteratively to see if the convergence condition
+    is still met.
+
+    TO DO: This function is not very optimal to obtain the spectrum vs phi, it diagonalizes too many times for already converged results.
+    '''
+
+    if not trunc_nums:
+        trunc_nums = [1] * circuit.n
+
+    ΔE = 1
+    E = []
+    trunc_nums_list = []
+    while ΔE > threshold:
+        trunc_nums = [n + 1 for n in trunc_nums]
+        circuit.set_trunc_nums(trunc_nums)
+        E.append(circuit.diag(n_eig)[0])
+        trunc_nums_list.append(trunc_nums.copy())
+        if len(E) == 1:
+            continue
+
+        ΔE = np.abs((E[-1] - E[-2]) / E[-2]).max()
+
+    E_conv = E[-1].copy()
+
+    if refine:
+        modes_to_refine = np.array([True] * circuit.n)
+        while np.any(modes_to_refine) == True:
+            for mode in range(circuit.n):
+                if modes_to_refine[mode] == False:
+                    continue
+                trunc_nums[mode] -= 1
+                circuit.set_trunc_nums(trunc_nums)
+                E.append(circuit.diag(n_eig)[0])
+                trunc_nums_list.append(trunc_nums.copy())
+                ΔE = np.abs((E[-1] - E_conv) / E_conv).max()
+                if ΔE < threshold:
+                    if trunc_nums[mode] == 1:
+                        modes_to_refine[mode] = False
+                    continue
+                else:
+                    modes_to_refine[mode] = False
+                    trunc_nums[mode] += 1
+                    del E[-1], trunc_nums_list[-1]
+                    ΔE = np.abs((E[-1] - E_conv) / E_conv).max()
+
+
+    if plot:
+        print(trunc_nums, E[-1],  ΔE )
+        fig, ax = plt.subplots()
+        ax.plot(np.abs(E_conv-np.array(E[1:])))
+        ax.set_yscale('log')
+        ax.set_ylabel('E')
+        labels_trunc_nums = [str(l) for l in trunc_nums_list]
+        ax.set_xlabel('trunc_nums')
+        ax.set_xticks(np.arange(len(E)),labels_trunc_nums, rotation=-60)
+        fig.show()
+
+    circuit.set_trunc_nums(trunc_nums)
+    return circuit, E_conv
---
+++ Tests/test_truncation_convergence.py
@@ -0,0 +1,53 @@
+import Modules.SQcircuit_extensions as sq_ext
+import SQcircuit as sq
+import numpy as np
+import matplotlib.pyplot as plt
+import importlib
+
+importlib.reload(sq_ext)
+plt.rcParams['backend'] = 'Qt5Agg'
+
+# %% Define circuit for testing
+# Circuit parameters
+Csh = 15
+C   = 15
+CJ  = 3
+Cg  = 20
+Lq = 25
+Lr = 10
+Δ  = 0.1
+EJ  = 10.0
+
+# Initialize loop(s)
+loop = sq.Loop(0.0)  # "Value" corresponds to phiExt / phi0 threading the loop (can change later)
+
+# Circuit elements
+C_01 = sq.Capacitor(C, 'fF')
+C_02 = sq.Capacitor(C, 'fF')
+C_12 = sq.Capacitor(CJ+Csh, 'fF')
+C_04 = sq.Capacitor(Cg, 'fF')
+
+L_03 = sq.Inductor(Lr, 'nH')
+L_31 = sq.Inductor(Lq/2 - Δ, 'nH', loops=[loop])
+L_23 = sq.Inductor(Lq/2 + Δ, 'nH', loops=[loop])
+
+JJ = sq.Junction(EJ, 'GHz', loops=[loop])
+
+# Create the circuit
+elements = {
+    # (0,4): [C_04],
+    (0, 3): [L_03],
+    (0, 1): [C_01],
+    (0, 2): [C_02],
+    (3, 1): [L_31],
+    (1, 2): [C_12, JJ],
+    (2, 3): [L_23],
+}
+
+circuit = sq.Circuit(elements)
+
+# %% Test truncation_convergence
+n_eig = 5
+loop.set_flux(0)
+trunc_nums = [5] * circuit.n
+circuit = sq_ext.truncation_convergence(circuit, n_eig, refine=True, plot=True)
